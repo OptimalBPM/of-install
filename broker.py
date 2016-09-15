ce65028ce3a9a1c7eafa35d22048b30b3f26966f
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-09-16 01:01:13 +0200

    Improve source documentation

--- broker.py
+++ broker.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+    This script initiates the Optimal Framework database with a base structure
+"""
+
 from of.broker.broker import start_broker
 
 start_broker()
--- broker.sh
+++ broker.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# This script starts the Optimal Framework broker
+
 python3 broker.py
