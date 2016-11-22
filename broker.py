6c09ffa2f57531148a26ba6e3240220f93b1a9fd
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-11-22 02:20:19 +0100

    Use local config

--- broker.py
+++ broker.py
@@ -2,7 +2,8 @@
 """
     This script initiates the Optimal Framework database with a base structure
 """
-
+import os
+script_dir = os.path.dirname(__file__)
 from of.broker.broker import start_broker
 
-start_broker()
+start_broker(_cfg_filename=os.path.join(script_dir, "config.json"))
